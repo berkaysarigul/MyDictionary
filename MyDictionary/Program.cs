d9f79a8fe21954ca08194e04f946e9fd715c8ab0
beko5 <beko5@DESKTOP-GUEE677>
2021-01-26 19:30:11 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+
+            myDictionary.Add("Berkay", 17);
+            myDictionary.Add("Ali",15);
+            myDictionary.Add("Hasan",21);
+
+            myDictionary.List();
+        }
+    }
+}
